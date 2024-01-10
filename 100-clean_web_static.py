164126eb6fcace53a43840eb4b1478ddd6a45081
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-01-10 18:09:14 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,7 +7,7 @@ from web_static content folder
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['35.227.5.245', '3.236.14.41']
+env.hosts = ['100.24.206.77', '54.237.45.137']
 
 
 def local_clean(number=0):
