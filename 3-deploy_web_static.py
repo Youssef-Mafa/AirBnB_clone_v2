d464794d14024236caaf2dee7deadb331991094e
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-01-10 18:07:41 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from web_static content folder
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['34.236.171.16', '3.237.45.190']
+env.hosts = ['100.24.206.77', '54.237.45.137']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
