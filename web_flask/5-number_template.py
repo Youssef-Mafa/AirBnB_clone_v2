1ef120fb009287bf69ff556495c18e999493c104
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-03-23 15:33:27 +0000

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,47 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application:
-listening on 0.0.0.0, port 5000
-with Fifth routes
-"""
-from flask import Flask, escape, request
-from flask import render_template
+"""start a flack server listening on port 5000 handling '/'"""
+from flask import Flask, render_template
+from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """ First Route that display Hello HBNB"""
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """handling / route"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """ Second Route that display HBNB"""
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """handling hbnb route"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def hello_text(text):
-    """ Third Route that display C and text"""
-    return "C {}".format(text.replace("_", " "))
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """handling varible routes"""
+    return f"C {escape(text.replace('_', ' '))}"
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def hello_python(text):
-    """ Fourth Route that display Python and text """
-    return "Python {}".format(text.replace("_", " "))
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """handling varible routes"""
+    return f"Python {escape(text.replace('_', ' '))}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def hello_number(n):
-    """ Fifth Route that display Number """
-    return "%d is a number" % n
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_a_number(n):
+    """handling numbers only"""
+    return f"{escape(n)} is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def hello_html(n):
-    """ Route that display HTML """
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """handling number and pass it to html template"""
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == '__main__':
+    """start the server"""
     app.run(host='0.0.0.0', port=5000)
