d35c7ce2a303f1710f22b8eed806d5f1aad48b3d
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-03-23 15:34:45 +0000

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""List of states on HTML states_list"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask
-from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities():
-    """Returns a rendered html template
-    at the /cities_by_states route,
-    listing the cities by states"""
-    return render_template('8-cities_by_states.html',
-                           states=storage.all('State').values())
+def cities_by_states():
+    """display a HTML page: (inside the tag BODY)"""
+    # sort states by name from a to z
+    slist = sorted(storage.all(
+        State).values(), key=lambda x: x.name)
+    # sort cities by name from a to z
+    for s in slist:
+        s.cities.sort(key=lambda x: x.name)
+    return render_template("8-cities_by_states.html", sorted_states_list=slist)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+def terminate(exc):
+    """close the storage"""
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    """start the server"""
+    app.run(host='0.0.0.0', port=5000)
