cf9b8e4d4ed049f2fd848a962130c1f88f070ccd
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-03-23 15:35:12 +0000

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""List of states on HTML states_list"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask
-from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def states_1(id=None):
-    """Returns a rendered html template:
-    if id is given, list the cities of that State
-    else, list all States
-    """
-    states = storage.all('State')
-    if id:
-        key = '{}.{}'.format('State', id)
-        if key in states:
-            states = states[key]
-        else:
-            states = None
+@app.route('/states', strict_slashes=False)
+def states_and_state(id=None):
+    """display a HTML page: (inside the tag BODY)"""
+    # sort states by name from a to z
+    slist = sorted(storage.all(
+        State).values(), key=lambda x: x.name)
+    if id is None:
+        return render_template("9-states.html", sorted_states_list=slist,
+                               states=True)
     else:
-        states = storage.all('State').values()
-    return render_template('9-states.html', states=states, id=id)
+        # sort cities by name from a to z
+        for s in slist:
+            if s.id == id:
+                s.cities.sort(key=lambda x: x.name)
+                return render_template("9-states.html", state=s)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+def terminate(exc):
+    """close the storage"""
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    """start the server"""
+    app.run(host='0.0.0.0', port=5000)
